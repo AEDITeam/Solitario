c9fcc73f886c18164beb90ab34819b1b38d5ae67
root <root@DESKTOP-V00N9DP.localdomain>
2020-03-13 16:04:54 +0100

    Plantilla creada

---
+++ Solitario/src/solitario/Core/Carta.java
@@ -0,0 +1,12 @@
+/*
+ * Representa una carta, formada por un número y su palo correspondiente
+ */
+package solitario.Core;
+
+/**
+ *
+ * @author juanm
+ */
+public class Carta {
+    
+}
---
+++ Solitario/src/solitario/IU/Main.java
@@ -0,0 +1,18 @@
+
+package solitario.IU;
+
+import static solitario.IU.Solitario.inicioPartida;
+
+
+/**
+ *
+ * @author juanm
+ */
+public class Main {
+    
+    public static void main(String[] args) {              
+                inicioPartida();
+           
+}
+    
+}
