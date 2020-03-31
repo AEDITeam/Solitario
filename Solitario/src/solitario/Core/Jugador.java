cbd2c327f5382ed44c48229913b7c676b4e2e8e7
Cedricpideca <62180578+Cedricpideca@users.noreply.github.com>
2020-03-31 18:32:38 +0200

    Update Jugador.java
    yo y juanma estamos preparando la función, nos coordinaremos con mesa para las comprobaciones de movimientos y que movimiento es, finalmente añadiremos los movimientos

--- Solitario/src/solitario/Core/Jugador.java
+++ Solitario/src/solitario/Core/Jugador.java
@@ -11,4 +11,81 @@ package solitario.Core;
  */
 public class Jugador {
     
+    
+    public boolean existeMovimiento(){
+    
+    if(Comprobar movimiento == 0){
+    
+        System.out.println("No ha movimientos  a realizar, el juego ha finalizado");
+    
+    
+    }
+    
+    else{
+            
+            // Invocamos función que nos permita continuar con el movimiento
+            
+            }
+    return existeMovimiento;
+    }
+    
+    
+    public boolean darVuelta(){
+    
+    if(cartaVolteable==1){
+    
+    // Invocamos función de voltear carta
+    
+    
+    }
+    
+    else{
+    
+    // Invocamos función de enviar a la pila de descartes
+    
+    
+    }
+    
+    return darVuelta;
+    }
+    
+    
+    public boolean descartar(){
+    
+    if(cartaDescartable == 1){
+    
+    
+    // Invocamos función de mover a la pila de descartes
+    
+    
+    
+    
+    }
+    
+    
+    return darVuelta;
+    
+    }
+    
+    
+    public boolean moverAOtroMonton(){
+    
+    
+    if (cartaLibre == 1){
+
+
+        // Invocamos función de mover la carta a otro que se pueda mover
+    
+
+}    
+    }   
+    
+    /* Aquí escribimos las funciones con las que trabajaremos que no correspondan a mesa
+    estas funciones pueden ser de distintos tipos y nos ayudarán a realizar estos movimientos, 
+    con ellas recorreremos el array de cartas (bucles for) y observaremos cuales son volteables
+    culaes son descartables y cuales están libres.
+    */ 
+    
+    
+    
 }
